045f8956ff77b85ac482ac41b074c14d31768bb8
Anas Mammeri <anas.mammeri@gmail.com>
2017-06-30 16:52:35 +0200

    feat: creation of the Login page. step 1.1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Hello from '@/components/Hello'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
