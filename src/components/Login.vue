86f48ab823f774196922a430a4c27d3ac30908bd
Anas Mammeri <anas.mammeri@gmail.com>
2017-07-05 14:14:06 +0200

    feat: sign up with firebase. step 3.3

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="login">
     <h3>Sign In</h3>
-    <input type="text" placeholder="Email"><br>
-    <input type="password" placeholder="Password"><br>
+    <input type="text" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
     <button v-on:click="signIn">Connection</button>
     <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
   </div>
 </template>
 
 <script>
+  import firebase from 'firebase'
+
   export default {
     name: 'login',
     data: function() {
-      return {}
+      return {
+        email: '',
+        password: ''
+      }
     },
     methods: {
       signIn: function() {
-        this.$router.replace('hello')
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+          function(user) {
+            alert('Well done ! You are now connected')
+          },
+          function(err) {
+            alert('Oops. ' + err.message)
+          }
+        );
       }
     }
   }
