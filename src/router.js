098faebe363b5b994a487c696e42e1c06b778566
Anas Mammeri <anas.mammeri@gmail.com>
2018-11-08 18:06:13 +0100

    feat: creation of the SignUp page. step 1.2

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Login from './views/Login.vue';
+import SignUp from './views/SignUp.vue';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/sign-up',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 });
