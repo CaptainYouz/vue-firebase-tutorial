9e4abb1763986fc78fa1dd4b003177eae5d2d4e1
Anas Mammeri <anas.mammeri@gmail.com>
2018-11-08 18:49:47 +0100

    feat: integration of firebase. step 3.1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import firebase from 'firebase';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+  databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+  messagingSenderId: 'YOUR_MESSAGING_SEND_ID'
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
