af78e69005a62face55e7699f02e9c9450934703
Anas Mammeri <anas.mammeri@gmail.com>
2018-11-10 16:01:46 +0100

    feat: create a new account on firebase. step 3.2

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="sign-up">
     <p>Let's create a new account !</p>
-    <input type="text" placeholder="Email"><br>
-    <input type="password" placeholder="Password"><br>
-    <button>Sign Up</button>
+    <input type="text" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
+    <button @click="signUp">Sign Up</button>
     <span>or go back to <router-link to="/login">login</router-link>.</span>
   </div>
 </template>
 
  <script>
+  import firebase from 'firebase';
+
   export default {
     name: 'signUp',
     data() {
-      return {}
+      return {
+        email: '',
+        password: ''
+      }
     },
-    methods: {}
+    methods: {
+      signUp: function() {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+          function (user) {
+            alert('Your account has been created !')
+          },
+          function (err) {
+            alert('Oops. ' + err.message)
+          }
+        );
+      }
+    }
   }
 </script>
 
