916867ae58d2025509e7a14a4dafa7e306e77180
Anas Mammeri <anas.mammeri@gmail.com>
2018-11-30 16:09:12 +0100

    feat: create the html/css. 1.1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,6 +4,12 @@
     <input type="text" v-model="email" placeholder="Email"><br>
     <input type="password" v-model="password" placeholder="Password"><br>
     <button @click="login">Connection</button>
+    <p>
+      or Sign In with Google <br>
+      <button class="social-button">
+        <img alt="Google Logo" src="../assets/google-logo.png">
+      </button>
+    </p>
     <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
   </div>
 </template>
@@ -55,5 +61,20 @@
   p a {
     text-decoration: underline;
     cursor: pointer;
+  }
+  .social-button {
+    width: 75px;
+    background: white;
+    padding: 10px;
+    border-radius: 100%;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
+    outline: 0;
+    border: 0;
+  }
+  .social-button:active {
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1);
+  }
+  .social-button img {
+    width: 100%;
   }
 </style>
